3f83dcf3e2274fc17198c92da72a058c6aeefaf1
Aaron Shaver <aaronshaverpdx@gmail.com>
2021-02-01 11:37:19 -0800

    local commit

---
+++ src/main/java/com/aaronshaver/minikafka/MinikafkaApplication.java
@@ -0,0 +1,13 @@
+package com.aaronshaver.minikafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinikafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinikafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaronshaver/minikafka/MinikafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaronshaver.minikafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinikafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
